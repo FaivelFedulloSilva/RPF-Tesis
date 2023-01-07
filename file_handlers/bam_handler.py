354c074de5e49600c2635f07537c3b63d52a5c09
Faivel <faivel.f.s@gmail.com>
2023-01-07 16:10:16 -0300

    Add CoveragePerFeature and CoveragePlot
    Now it is possible to get sequencing depth per base per transcript. Also it is possible to visualize the sequencing depth per feature as a bar plot with range slider based in plotly

--- file_handlers/bam_handler.py
+++ file_handlers/bam_handler.py
@@ -3,17 +3,41 @@ import pysam
 
 class BAMhandler:
     def __init__(self, path, options) -> None:
-        self.__path = path
-        
+        self._path = path
+    
         def _read_file(path, options):
             if options == 'b':
-                pysam.AlignmentFile(path, 'rb')
+                return pysam.AlignmentFile(path, 'rb')
             elif options == 's':
-                pysam.AlignmentFile(path, 'rs')
+                return pysam.AlignmentFile(path, 'rs')
 
         
-        self.__bam_object = _read_file(path, options)
-    
+        # self._bam_object = _read_file(path, options)
+
+    def region_coverage(self, contig: str, start: int, end: int):
+        coverage = pysam.depth(self._path, '-a', '-r', f"{contig}:{start}-{end}")
+        coverage = coverage.split('\n')
+        coverage_return = []
+        for line in coverage[:-1]:
+            splitted_line = line.split('\t')
+            coverage_return.append([splitted_line[1], splitted_line[2]])
+        return coverage_return
+
+RPF_PATH = './Data/RPF_1/accepted_hits_01.bam'
+TOTAL_PATH = './Data/totalRNA_01/accepted_hits_01.bam'
+
+
+contig = "chr15"
+start = 30903852
+end = 30903887
+regio = f"{contig}:{start}-{end}"
+bam = BAMhandler(RPF_PATH, 'b')
+print(bam.region_coverage(contig, start, end))
+# counted = bam._bam_object.count(region=regio)
+# print(counted)
+# count_coverage = bam._bam_object.pileup(contig, start, end)
+# for i in count_coverage:
+#     print('->', i)
 
     
 
--- main.py
+++ main.py
@@ -3,16 +3,53 @@ import matplotlib as plt
 import pysam
 from file_handlers import GTFhandler
 from file_handlers import FastaHandler
+from file_handlers import BAMhandler
+import pandas as pd
 import polars as pl
 from polars import DataFrame
 from libs.DNAStringSet import DNAStringSet
+
+import plotly.graph_objects as go
+import plotly.express as px
+
+
+import numpy as np
+
+from bokeh.layouts import column
+from bokeh.models import ColumnDataSource, RangeTool
+from bokeh.plotting import figure, show, output_file, save
 
 RPF_PATH = './Data/RPF_1/accepted_hits_01.bam'
 TOTAL_PATH = './Data/totalRNA_01/accepted_hits_01.bam'
 REF_PATH = 'Data/reference/hg38.fa'
 GTF_PATH = 'Data/genesFiltrada.gtf'
 
-def get_base_sequence(gtf: DataFrame, dna: DNAStringSet = None):
+
+def plot_with_plotly(cov: list[int]):
+    print(cov)
+    position = np.arange(0, len(cov))
+    covdf = {
+        'location': position,
+        'coverage': cov   
+    }
+    covdf = pd.DataFrame(covdf)
+
+    fig = px.bar(covdf, x='location', y='coverage', title='Time Series with Rangeslider')
+
+    fig.update_xaxes(rangeslider_visible=True)
+    fig.show()
+
+
+
+
+def get_second_elements(l: list[str,str])->list[int]:
+    new_list = []
+    for elem in l:
+        new_list.append(int(elem[1]))
+
+    return new_list
+
+def get_base_sequence(gtf: DataFrame, dna: DNAStringSet = None, bam: BAMhandler = None):
     print(gtf.sample(20))
     q = (gtf
             .lazy()
@@ -27,17 +64,24 @@ def get_base_sequence(gtf: DataFrame, dna: DNAStringSet = None):
     feature_sequences = {}
     for row in iter_df:
         feature_seq = []
+        feature_depth = []
         ref_seq = dna.get_sequence(row.seqname)
         for index in range(len(row.start)):
             seq = ref_seq.get_sub_sequence(row.start[index], row.end[index])
             if row.strand[index] == '-':
                 seq = seq.reverse_complement()
+            print(row.seqname, row.start[index], row.end[index])
+            cov = bam.region_coverage(row.seqname, row.start[index], row.end[index])
+            feature_depth += get_second_elements(cov)
             feature_seq.append(seq.get_as_string())
-        feature_sequences[row.transcript_id] = ''.join(feature_seq)
+        feature_sequences[row.transcript_id] = [''.join(feature_seq), feature_depth]
     for key, value in feature_sequences.items():
+        plot_with_plotly(value[1])
+        input()
         print(key, " -> ", value)
 
 gtf = GTFhandler(GTF_PATH)
 filtered_gtf = gtf.filter_by_feature('exon')
 dna = FastaHandler(REF_PATH)
-get_base_sequence(filtered_gtf, dna.get_reference())
+bam = BAMhandler(RPF_PATH, 'b')
+get_base_sequence(filtered_gtf, dna.get_reference(), bam)
